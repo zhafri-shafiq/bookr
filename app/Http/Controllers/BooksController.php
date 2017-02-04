dda69aac23cc5868a7acce1b8f2e39540148b5c5
zhafri-shafiq <skyisthelimit175@ymail.com>
2017-02-04 11:49:39 +0000

    initial commit

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+/**
+* Class BooksController
+* @package App\Http\Controllers
+*/
+
+class BooksController
+{
+	/**
+	* GET /books
+	* @return array
+	*/
+	
+	public function index()
+	{
+		return Book::all();
+	}
+
+	/**
+	* GET /books/{id}
+	* @param integer $id
+	* @return mixed
+	**/
+	public function show($id)
+	{
+		try {
+			return Book::findOrFail($id);
+		} catch (ModelNotFoundException $e) {
+			return response()->json([
+				'error' => [
+					'message' => 'Book not found'
+				]
+			], 404);
+		}
+	}
+}
